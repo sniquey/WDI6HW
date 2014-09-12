b9f6f3ceaf1466763bb8b1ae13dc08c1f83c09a1
Veronique <veronique.eldridge@gmail.com>
2014-09-12 16:48:25 +1000

    there is still an error fixing the cross line travel

--- MTA-Lab.rb
+++ MTA-Lab.rb
@@ -19,9 +19,9 @@ times = "Times Square"
 $subway_station = [stop_1, stop_3, stop_6, stop_8n, stop_8l, stop_23n, stop_236, stop_28n, stop_286, stop_33, stop_34, int, ast, times] 
 
 # arrays of stops for each of the 3 lines
-$line_n = [times, stop_34, stop_28n, stop_23n, int, stop_8n]
-$line_l = [stop_8l, stop_6, int, stop_3, stop_1]
-$line_6 = [stop_33,stop_286,stop_236, int, ast]
+line_n = [times, stop_34, stop_28n, stop_23n, int, stop_8n]
+line_l = [stop_8l, stop_6, int, stop_3, stop_1]
+line_6 = [stop_33,stop_286,stop_236, int, ast]
 
 
 # define subway hash that links keys of the lines to values of the arrays of stops corresponding to the lines
@@ -65,134 +65,147 @@ end
 # Get starting point and end point from user
 menu
 print "Please select your starting point: "
-get_on = gets.to_i
+$get_on = gets.to_i
 print "Please select your end point: "
-get_off = gets.to_i
+$get_off = gets.to_i
 
-def travel(point1, point2)
-	if ($line_n.include? $subway_station[point1 - 1]) && ($line_n.include? $subway_station[point2 - 1])
+def travel(line)
 		puts "There is a direct train to your destination!"
-		puts "Board the train at #{$subway_station[point1 - 1]}"
-		start_point = $line_n.index $subway_station[point1 - 1]
-		end_point = $line_n.index $subway_station[point2 - 1]
+		puts "Board the train at #{$subway_station[$get_on - 1]}"
+		start_point = line.index $subway_station[$get_on - 1]
+		end_point = line.index $subway_station[$get_off - 1]
 		if start_point < end_point
 			next_stop = start_point + 1
 			while next_stop < end_point
-			puts "Train goes through #{$line_n[next_stop]} station"
+			puts "Train goes through #{line[next_stop]} station"
 			next_stop += 1
 			end
-			puts "Get off at the next stop which is your destination: #{$subway_station[point2 -1]}."
+			puts "Get off at the next stop which is your destination: #{$subway_station[$get_off -1]}."
 		end
 		if end_point < start_point
 			next_stop = start_point + 1
 			while next_stop > end_point
-			puts "Train goes through #{point1[next_stop]} station"
+			puts "Train goes through #{$get_on[next_stop]} station"
 			next_stop -= 1
 			end
-			puts "Get off at the next stop which is your destination: #{$subway_station[point2 -1]}."
+			puts "Get off at the next stop which is your destination: #{$subway_station[$get_off -1]}."
 		end
+end
+
+# case for start on some line and end on a different one
+def travel_diff(line1, line2)
+	if (line1.include? $subway_station[$get_on - 1]) && (line2.include? $subway_station[$get_off - 1])
+		puts "You will need to change trains at UNION SQUARE!"
+		puts "Board the train at #{$subway_station[$get_on - 1]}"
+		start_point = line1.index $subway_station[$get_on - 1]
+
+		###### STARTING LINE IS WHICH?
+		if line1 == line_n
+			union_point = 4
+
+			end_point = line2.index $subway_station[$get_off - 1]
+			if start_point < union_point
+				next_stop = start_point + 1
+				while next_stop < union_point
+				puts "Train goes through #{line1[next_stop]} station"
+				next_stop += 1
+				end
+			end
+
+			puts "Get off at the next stop to change to Line L: #{line1[4]}."
+
+		elsif line1 == line_l
+			union_point = 2
+
+			end_point = line2.index $subway_station[$get_off - 1]
+			if start_point < union_point
+				next_stop = start_point + 1
+				while next_stop < union_point
+				puts "Train goes through #{line1[next_stop]} station"
+				next_stop += 1
+				end
+			end
+
+			puts "Get off at the next stop to change to Line L: #{line1[2]}."
+
+		elsif line1 == line_6
+			union_point = 3
+
+			end_point = line2.index $subway_station[$get_off - 1]
+			if start_point < union_point
+				next_stop = start_point + 1
+				while next_stop < union_point
+				puts "Train goes through #{line1[next_stop]} station"
+				next_stop += 1
+				end
+
+				puts "Get off at the next stop to change to Line L: #{line1[3]}."
+			end
+		end
+		
+		start_point = line2.index int
+		next_stop = start_point + 1
+
+		while next_stop < end_point
+			puts "Train goes through #{line1[next_stop]} station"
+			next_stop += 1
+		end
+
+		puts "Get off at the next stop which is your destination: #{$subway_station[$get_off -1]}."
 	end
 end
 
 
-if ((1..14).include? get_on) && ((1..14).include? get_off)
+if ((1..14).include? $get_on) && ((1..14).include? $get_off)
 	system("clear")
-	puts "You will be travelling from #{$subway_station[get_on - 1]} and arriving at #{$subway_station[get_off - 1]}"
+	puts "You will be travelling from #{$subway_station[$get_on - 1]} and arriving at #{$subway_station[$get_off - 1]}"
+	
 	#case for start and finish stops being on the same line
 	#case for travelling only on line N
-	if ($line_n.include? $subway_station[get_on - 1]) && ($line_n.include? $subway_station[get_off - 1])
-		travel(get_on, get_off)
-	end
-	if ($line_l.include? $subway_station[get_on - 1]) && ($line_l.include? $subway_station[get_off - 1])
-		travel(get_on, get_off)
-	end
-	if ($line_6.include? $subway_station[get_on - 1]) && ($line_6.include? $subway_station[get_off - 1])
-		travel(get_on, get_off)
+	if (line_n.include? $subway_station[$get_on - 1]) && (line_n.include? $subway_station[$get_off - 1])
+		travel(line_n)
 	end
 
-
-	# #case for travelling only on line L
-	# if (line_l.include? subway_station[get_on - 1]) && (line_l.include? subway_station[get_off - 1])
-	# 	puts "There is a direct train to your destination!"
-	# 	puts "Board the train at #{subway_station[get_on - 1]}"
-	# 	start_point = line_l.index subway_station[get_on - 1]
-	# 	end_point = line_l.index subway_station[get_off - 1]
-	# 	if start_point < end_point
-	# 		next_stop = start_point + 1
-	# 		while next_stop < end_point
-	# 		puts "Train goes through #{line_l[next_stop]} station"
-	# 		next_stop += 1
-	# 		end
-	# 		puts "Get off at the next stop which is your destination: #{subway_station[get_off -1]}."
-	# 	end
-	# 	if end_point < start_point
-	# 		next_stop = start_point + 1
-	# 		while next_stop > end_point
-	# 		puts "Train goes through #{line_l[next_stop]} station"
-	# 		next_stop -= 1
-	# 		end
-	# 		puts "Get off at the next stop which is your destination: #{subway_station[get_off -1]}."
-	# 	end
-	# end
-	# #case for travelling only on line 6
-	# if (line_6.include? subway_station[get_on - 1]) && (line_6.include? subway_station[get_off - 1])
-	# 	puts "There is a direct train to your destination!"
-	# 	puts "Board the train at #{subway_station[get_on - 1]}"
-	# 	start_point = line_6.index subway_station[get_on - 1]
-	# 	end_point = line_6.index subway_station[get_off - 1]
-	# 	if start_point < end_point
-	# 		next_stop = start_point + 1
-	# 		while next_stop < end_point
-	# 		puts "Train goes through #{line_6[next_stop]} station"
-	# 		next_stop += 1
-	# 		end
-	# 		puts "Get off at the next stop which is your destination: #{subway_station[get_off -1]}."
-	# 	end
-	# 	if end_point < start_point
-	# 		next_stop = start_point + 1
-	# 		while next_stop > end_point
-	# 		puts "Train goes through #{line_6[next_stop]} station"
-	# 		next_stop -= 1
-	# 		end
-	# 		puts "Get off at the next stop which is your destination: #{subway_station[get_off -1]}."
-	# 	end
-	# end
-
-#case for start on line n and end on line l
-	if ($line_n.include? $subway_station[get_on - 1]) && ($line_l.include? $subway_station[get_off - 1])
-		puts "You will need to change trains at UNION SQUARE!"
-		puts "Board the train at #{subway_station[get_on - 1]}"
-		start_point = line_n.index subway_station[get_on - 1]
-		union_point = 4
-		end_point = line_l.index subway_station[get_off - 1]
-		if start_point < union_point
-			next_stop = start_point + 1
-			while next_stop < union_point
-			puts "Train goes through #{line_n[next_stop]} station"
-			next_stop += 1
-			end
-		end
-		puts "Get off at the next stop to change to Line L: #{line_n[4]}."
-		start_point = line_l.index int
-		next_stop = start_point + 1
-		while next_stop < end_point
-			puts "Train goes through #{line_n[next_stop]} station"
-			next_stop += 1
-		end
-		puts "Get off at the next stop which is your destination: #{subway_station[get_off -1]}."
+	#case for travelling only on line L
+	if (line_l.include? $subway_station[$get_on - 1]) && (line_l.include? $subway_station[$get_off - 1])
+		travel(line_l)
 	end
-#case for start on line n and end on line 6
 
-# case for start on line l and end on line 6
-
-# case for start on line l and end on line n
-
-# case for start on line 6 and end on line n
-
-# case for start on line 6 and end on line l
+	#case for travelling only on line 6
+	if (line_6.include? $subway_station[$get_on - 1]) && (line_6.include? $subway_station[$get_off - 1])
+		travel(line_6)
+	end
 
 
+	#case for start on line_n and end on line_l
+	if (line_n.include? $subway_station[$get_on - 1]) && (line_l.include? $subway_station[$get_off - 1])
+		travel_diff(line_n, line_l)
+	end
 
+	#case for start on a line_n and end on line_6
+	if (line_n.include? $subway_station[$get_on - 1]) && (line_6.include? $subway_station[$get_off - 1])
+		travel_diff(line_n, line_6)
+	end
+
+	#case for start on line_l and end on line_n
+	if (line_l.include? $subway_station[$get_on - 1]) && (line_n.include? $subway_station[$get_off - 1])
+		travel_diff(line_l, line_n)
+	end
+
+	#case for start on a line_l and end on line_6
+	if (line_l.include? $subway_station[$get_on - 1]) && (line_6.include? $subway_station[$get_off - 1])
+		travel_diff(line_l, line_6)
+	end
+
+	#case for start on a line_6 and end on line_n
+	if (line_6.include? $subway_station[$get_on - 1]) && (line_n.include? $subway_station[$get_off - 1])
+		travel_diff(line_6, line_n)
+	end
+
+	#case for start on a line_6 and end on line_l
+	if (line_6.include? $subway_station[$get_on - 1]) && (line_l.include? $subway_station[$get_off - 1])
+		travel_diff(line_6, line_l)
+	end
 else
 	puts "WTF HAS HAPPENED."
 end
