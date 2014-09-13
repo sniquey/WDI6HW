f041b4b110ce95d6e2c6c4e3ac72f05fc781d08b
Veronique <veronique.eldridge@gmail.com>
2014-09-13 20:16:37 +1000

    rental

--- rental/main.rb
+++ rental/main.rb
@@ -6,11 +6,15 @@ require_relative './tenant.rb'
 require_relative './apartment.rb'
 require_relative './building.rb'
 
-chris = Tenant.new("Chris Eccleston", 23, "male")
+chris = Tenant.new "Chris Eccleston", 23, "male"
+sophie = Tenant.new "Sophie Walden", 25, "female"
+helen = Tenant.new "Helen Fitz", 27, "female"
 
-apt = Apartment.new('25b',123, 2, 15)
+apt = Apartment.new '25b',123, 2, 15
+apt2 = Apartment.new '28c', 12, 2, 1
 
-building = Building.new('123 Fake st', 'Neo-industrial', true, false, 1000)
+building = Building.new '123 Fake st', 'Neo-industrial', 't', 'f', 1000
+building2 = Building.new '222 Simple lane', 'Art Deco', 'f', 'f', 300
 
 # Globals to store our data
 $buildings = []
@@ -19,28 +23,43 @@ $apartments = []
 
 
 # Seed data
-$buildings.push building
-$buildings.first.apartments[apt.name] = apt
-$buildings.first.apartments[apt.name].tenants << chris
+# $buildings.first.apartments[apt.name] = apt
+apt.tenants << chris << sophie << helen
+$apartments << apt << apt2
+building.apartments[apt.name] = apt
+building2.apartments[apt2.name] = apt2
+$buildings << building << building2
+
 
 # Greeting
 puts "Welcome to Find-A-Place!"
 
 def main_menu
 	puts "What are you after:"
-	puts "1. List an apartment for rent"
-	puts "2. Find an apartment for rent"
+	puts "1. List an apartment"
+	puts "2. Find an apartment"
 	# puts "1. List all apartments"
 	puts "q. Quit"
 end
 
+def sub_menu_2
+	puts "FIND AN APARTMENT"
+	puts "1. List all apartments"
+	puts "2. List all buildings"
+end
 
 def read_main_selection
 	main_menu
 	gets.chomp.downcase
 end
 
+def read_sub_menu_2
+	sub_menu_2
+	gets.to_i	
+end
+
 main_menu_option = read_main_selection
+sub_menu_2_option = read_sub_menu_2
 
 until main_menu_option == 'q'
 	case main_menu_option
@@ -48,6 +67,7 @@ until main_menu_option == 'q'
 		# Find out apartment attributes
 		# system('clear')
 		# def list_building_menu
+
 			puts "Please enter the building information:"
 			print "What is the address? "
 			address = gets.chomp
@@ -59,19 +79,23 @@ until main_menu_option == 'q'
 			has_doorman = gets.chomp
 			print "How many floors are there in the building? "
 			num_floors = gets.to_i
-			building = Building.new(address, style, has_elevator, has_doorman, num_floors)
-		# list_building_menu
-		puts "#{@address} and #{@style} and #{@has_doorman} and #{@num_floors}"
+			$buildings << Building.new(address, style, has_elevator, has_doorman, num_floors)
+			puts "#{$buildings.last}"
+		# # list_building_menu
+	
 	# end
 	when '2'
-		# List all apartments.
-		$buildings.each do |b|
-			b.apartments.each do |number, apartment|
-				puts apartment
+		sub_menu_2_option
+		if result_sub_menu_2 == 1
+			# List all apartments.
+			$buildings.each do |b|
+				b.apartments.each do |number, apartment|
+					puts apartment
+				end
 			end
 		end
 
-		main_menu_option = read_main_selection
 	end
 end
 
+binding.pry
